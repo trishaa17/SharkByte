9bc491e70e2f3fe1334a3eede6bd676f458db607
trishaa17 <157675247+trishaa17@users.noreply.github.com>
2025-01-18 00:56:05 +0800

    Update next.config.ts

--- campus_cart/next.config.ts
+++ campus_cart/next.config.ts
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true, // Required for using the `src/app` directory
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
